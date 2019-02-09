b0d1851e1abbb16c2050c84d82ac489cee59ca48
kirito <manjrekarom@gmail.com>
2019-02-09 13:38:15 +0530

    Yelo code

---
+++ Python/control_rpi_led.py
@@ -0,0 +1,163 @@
+from __future__ import print_function
+import threading
+import time
+import json
+import ast
+import RPi.GPIO as GPIO
+import sys
+import Adafruit_DHT
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+# Details about thing
+# TODO: Change settings
+THING_ID = "thing82"
+CLIENT_ID = "MyRpi82"
+CERTIFICATE_PATH = "./certificates"
+ENDPOINT = "a221a6r4ojicsi-ats.iot.ap-southeast-1.amazonaws.com"
+
+# LED PINs
+# TODO: Change pins [if required]
+LED_PIN_1 = 14 # 11 R but B
+LED_PIN_2 = 15 # 13 G but G
+LED_PIN_3 = 18 # 15 B but R
+
+# Temperature and humidity sensor
+DHT_PIN = 10
+
+# Device ids
+LED_ID = "device166.389"
+
+TEMP_ID = "device167.390"
+HUMID_ID = "device167.391"
+
+
+# Initialize GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN_1, GPIO.OUT)
+GPIO.setup(LED_PIN_2, GPIO.OUT)
+GPIO.setup(LED_PIN_3, GPIO.OUT)
+
+# Change to your topics here
+UPDATE_TOPIC = "$aws/things/" + THING_ID + "/shadow/update"
+DELTA_TOPIC = "$aws/things/" + THING_ID + "/shadow/update/delta"
+
+ROOT_CA = CERTIFICATE_PATH + "/rootCA.pem"
+PRIVATE_KEY = CERTIFICATE_PATH + "/private.key.pem"
+CERTIFICATE_CRT = CERTIFICATE_PATH + "/certificate.crt.pem"
+
+sensor = Adafruit_DHT.DHT11
+
+
+def get_init_mqtt_client(): 
+	# Configuration for AWS IoT
+	myMQTTClient = AWSIoTMQTTClient(CLIENT_ID)
+	myMQTTClient.configureEndpoint(ENDPOINT, 8883)
+	myMQTTClient.configureCredentials(ROOT_CA, PRIVATE_KEY, CERTIFICATE_CRT)
+
+
+	# myMQTTClient.configureOfflinePublishQueueing(-1)  
+	# Infinite offline Publish queueing
+	# myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+	myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+	myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+	myMQTTClient.enableMetricsCollection()
+
+	return myMQTTClient
+
+
+
+def publish_continuously(interval, mqtt_client):
+	while True:
+		publish_sensor_data(mqtt_client)
+		time.sleep(interval)
+
+
+
+def publish_sensor_data(mqtt_client):	
+	# Dictionaries and messages for publishing
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT_PIN)
+	# TODO: Change the dictionary keys to keys of the device shadow JSON 
+	publish_dict = {'state': {'reported': {LED_ID: get_led_values(), 
+	TEMP_ID: temperature,
+	HUMID_ID: humidity
+	}}}
+
+	publish_message = json.dumps(publish_dict)
+	print("Message to publish:-", publish_message)
+
+	# Update device shadow
+	published = mqtt_client.publish(UPDATE_TOPIC, publish_message, 0)
+	print("Published:-", published)
+
+
+
+def update_thing_state(client, userdata, message):
+	print(client, userdata, message)
+	message_dict = json.loads(message.payload.decode('utf-8'))
+
+	state = message_dict['state']
+	print(state)
+
+	# TODO: Change the dictionary keys to keys of the device shadow JSON
+	# Change to your LED device 
+	print("len of arguments ", len(sys.argv[1:]))
+	if LED_ID in state:
+		led = message_dict['state'][LED_ID]
+		print(led)
+		if led == "BLUE":
+			GPIO.output(LED_PIN_1, False)
+			GPIO.output(LED_PIN_2, True)
+			GPIO.output(LED_PIN_3, True)
+		elif led == "GREEN":
+			GPIO.output(LED_PIN_1, True)
+			GPIO.output(LED_PIN_2, False)
+			GPIO.output(LED_PIN_3, True)
+		elif led == "RED":
+			GPIO.output(LED_PIN_1, True)
+			GPIO.output(LED_PIN_2, True)
+			GPIO.output(LED_PIN_3, False)
+		elif led == "OFF":
+			GPIO.output(LED_PIN_1, True)
+			GPIO.output(LED_PIN_2, True)
+			GPIO.output(LED_PIN_3, True)
+	
+	# push updated state
+	# publish_sensor_data(mqtt_client, servo1, servo2)
+
+
+def get_led_values():
+	if not GPIO.input(LED_PIN_1):
+		led = "BLUE"
+	elif not GPIO.input(LED_PIN_2):
+		led = "GREEN"
+	elif not GPIO.input(LED_PIN_3):
+		led = "RED"
+	else:
+		led = "OFF"
+	return led 
+
+
+
+if __name__ == "__main__":
+	try:
+		# Connect to MQTT broker
+		mqtt_client = get_init_mqtt_client()
+
+		print("Connected:-", mqtt_client.connect())
+		
+		# Start the thread
+		t = threading.Thread(target=publish_continuously, args = (5, mqtt_client))
+		
+
+		# Run the thread as a daemon which will die after parent dies 
+		t.daemon = True
+		t.start()
+		
+		mqtt_client.subscribe(DELTA_TOPIC, 0, update_thing_state)
+		
+		# To keep parent alive as long as ctrl + c
+		while True:
+			pass
+	except KeyboardInterrupt:
+		print("Cleaning up and exiting!")
+		GPIO.cleanup()
